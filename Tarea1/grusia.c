536f9cdefdbe41d02baab699b0133c07ad196c05
dvher <diego.vergara5@mail.udp.cl>
2022-03-31 17:23:41 -0300

    Advancement

--- Tarea1/grusia.c
+++ Tarea1/grusia.c
@@ -5,22 +5,26 @@
 #include <string.h>
 #include <time.h>
 #include <signal.h>
-#include <sys/wait.h>
 
 // Function to clean an array of PIDs
 void clean_array(pid_t[], int);
+// Function to check if the process is the parent
+int isParent(pid_t[], int);
+// Function to resume the process
+void resumeProcess(pid_t[], int, int);
 
 int main(void) {
     srand(time(NULL));
-    printf("PPID, PID: %d %d\n", getppid(), getpid());
-    // Variable que contiene el número de locaciones
+    // Number of locations
     int n = 0;
     // Create variable for iterations (C99)
     int i;
     // Ask for array's length
     printf("Ingrese el número de locaciones posibles de Zulensky: ");
     scanf("%d", &n);
-    // Arreglo que contiene los PIDs de las locaciones posibles de Zulensky
+    // Zulensky's location
+    int location = rand() % n;
+    // Array containing all possible locations of Zulensky
     pid_t possible_locations[n];
     // Fill array with zeros
     clean_array(possible_locations, n);
@@ -28,13 +32,26 @@ int main(void) {
     possible_locations[0] = fork();
     for(i = 1; i < n; i++) possible_locations[i] = possible_locations[i - 1] ? fork() : 0;
     
-    if(possible_locations[n - 1]) {
+    if(isParent(possible_locations, n)) {
         int found = 0;
+        int j = 0;
         while(!found) {
-            
+            if(j == location) {
+               found = 1;
+               printf("Zulensky ha sido encontrado.\n");
+               printf("Eliminando el resto de locaciones de la lista...\n");
+               kill(possible_locations[j], SIGKILL);
+               resumeProcess(possible_locations, n, j);
+            } else {
+               kill(possible_locations[j], SIGINT);
+               possible_locations[j] = 0;
+               //printf("Zulensky no se encontraba en esta locación, relocalizando...\n");
+               possible_locations[j] = fork();
+            }
+            j = (j + 1) % n;
         }
-    }
-    if(getpid() != getppid()) exit(0);
+    } else
+        kill(getpid(), SIGSTOP);
     return 0;
 }
 
@@ -42,5 +59,19 @@ void clean_array(pid_t arr[], int length) {
     int i;
     for(i = 0; i < length; i++) {
         arr[i] = 0;
+    }
+}
+
+int isParent(pid_t arr[], int length) {
+    int i;
+    for(i = 0; i < length; i++)
+        if(!arr[i]) return 0;
+    return 1;
+}
+
+void resumeProcess(pid_t arr[], int length, int dead) {
+    int i;
+    for(i = 0; i < length; i++) {
+        if(i != dead) kill(arr[i], SIGCONT);
     }
 }
